c52b9e4c90bb8eb5752cc0f4a7665935d0af17cf
oliwiazdziech-tech <oliwia.zdziech@gmail.com>
2026-02-02 17:18:51 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Highrise } = require("highrise.sdk");
+
+const bot = new Highrise({
+  token: process.env.BOT_TOKEN,
+  room: process.env.ROOM_ID,
+  events: ['ChatEvent', 'PlayerJoinEvent']
+});
+
+bot.on('ready', () => {
+  console.log('Bot is online!');
+});
+
+bot.on('chatCreate', async (user, message) => {
+  if (message.toLowerCase() === 'hello') {
+    await bot.chat(`Hey ${user.username}! 👋`);
+  }
+});
+
+bot.on('playerJoin', async (user) => {
+  await bot.chat(`Welcome ${user.username}! 🎉`);
+});
+
+bot.connect();
