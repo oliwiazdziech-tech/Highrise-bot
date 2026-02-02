7090f3fad486e9ed3d00838960d81a1419fb1b1e
oliwiazdziech-tech <oliwia.zdziech@gmail.com>
2026-02-02 18:14:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { Highrise } = require("highrise.bot");
 
-// Print variables to debug
-console.log("Token exists:", !!process.env.BOT_TOKEN);
-console.log("Room ID exists:", !!process.env.ROOM_ID);
-console.log("Room ID value:", process.env.ROOM_ID);
+// Get variables
+const myToken = process.env.token;
+const myRoom = process.env.room;
+
+console.log("Token exists:", !!myToken);
+console.log("Room exists:", !!myRoom);
 
 const bot = new Highrise(
   ["ChatEvent", "UserJoinedEvent", "UserLeftEvent"],
   {
-    token: process.env.BOT_TOKEN,
-    room: process.env.ROOM_ID
+    token: myToken,
+    room: myRoom
   }
 );
 
